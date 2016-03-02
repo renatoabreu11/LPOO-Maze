9cbd17e62ff4d051c6ae5c1edb0639030fc1439e
Evenilink <jose__carlos17@hotmail.com>
2016-03-02 13:27:48 +0000

    New test class with Dragon movement

---
+++ test/TestMazeWithDragonMovement.java
@@ -0,0 +1,80 @@
+package maze.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import maze.logic.Coordinates;
+import maze.logic.Game;
+import maze.logic.Dragon.DragonState;
+
+public class TestMazeWithDragonMovement {
+	
+	@Test
+	public void testDragonAleatoryMovement()
+	{
+		Game game = new Game(2);
+		
+		boolean moveUp = false;
+		boolean moveDown = false;
+		boolean moveLeft = false;
+		boolean moveRight = false;
+		
+		boolean dragonOnTop = false;
+		
+		//Tests if the dragon moves in all directions
+		while(!(moveUp && moveDown && moveLeft && moveRight))
+		{
+			Coordinates c = game.getDragon().getCoordinates();
+			game.UpdateGame("A");
+			
+			if(c.getX() - 1 == game.getDragon().getCoordinates().getX())
+				moveRight = true;
+			else
+				if(c.getX() + 1 == game.getDragon().getCoordinates().getX())
+					moveLeft = true;
+				else
+					if(c.getY() - 1 == game.getDragon().getCoordinates().getY())
+						moveDown = true;
+					else
+						if(c.getY() + 1 == game.getDragon().getCoordinates().getY())
+							moveUp = true;
+		}
+		
+		assertEquals(true, moveUp);
+		assertEquals(true, moveDown);
+		assertEquals(true, moveLeft);
+		assertEquals(true, moveRight);
+		
+		//Tests if the dragon can be on top of the sword
+		while(!game.getDragon().getDragonOnTop())
+			game.UpdateGame("A");
+		
+		assertEquals(true, game.getDragon().getDragonOnTop());
+	}
+	
+	@Test
+	public void testDragonSleepingState()
+	{
+		Game game = new Game(3);
+		
+		while(game.getDragon().getDragonState() != DragonState.sleeping)
+			game.UpdateGame("A");
+		
+		assertEquals(DragonState.sleeping, game.getDragon().getDragonState());
+	}
+	
+	@Test
+	public void testDragonWakingUpAfterSleeping()
+	{
+		Game game = new Game(3);
+		
+		while(game.getDragon().getDragonState() != DragonState.sleeping)
+			game.UpdateGame("A");
+		
+		while(game.getDragon().getDragonState() == DragonState.sleeping)
+			game.UpdateGame("A");
+		
+		assertNotEquals(DragonState.sleeping, game.getDragon().getDragonState());
+	}
+}
